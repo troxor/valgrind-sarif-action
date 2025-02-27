ca0403f4d62e845d3ab3e76c27397362176f68a8
Troy C. <327529+troxor@users.noreply.github.com>
2025-02-27 14:43:15 -0600

    Implement basic functionality and self-tests (#1)

--- vgxml-to-sarif.py
+++ vgxml-to-sarif.py
@@ -6,6 +6,7 @@ from jschema_to_python.to_json import to_json
 import json
 
 def parse_valgrind_xml(xml_file):
+    # Drawback: if there's one field, e.g. <stack>, <frame>, etc. it does not become a singleton list
     xdict = xmltodict.parse(open(xml_file, "r").read())
     # print(json.dumps(xdict))
     return xdict
@@ -17,30 +18,26 @@ def create_sarif(results):
     vgo = results["valgrindoutput"]
     tool = Tool(driver=ToolComponent(name=vgo["protocoltool"], version=vgo["protocolversion"], information_uri="https://valgrind.org"))
     results = []
+
+    # Each <error> corresponds to a Result
     if "error" in vgo.keys():
-        error = vgo["error"]
-        # If there's only one type of memory error, this won't be a list
-        if not isinstance(error, list):
-            error = [error]
-        # Each <error> corresponds to a Result
-        for e in error:
+        errors = vgo["error"] if isinstance(vgo["error"], list) else [vgo["error"]]
+        for e in errors:
             m = Message(text=e["kind"])
             result = Result(message=m, rule_id=f'{e["unique"]}', locations=[])
-            for frame in e["stack"]["frame"]:
-                pl = None
-                r = None
-                if "dir" in frame.keys() and "file" in frame.keys():
-                    al = ArtifactLocation(uri=f'file://{frame["dir"]}/{frame["file"]}')
-                    if "line" in frame.keys():
-                        r = Region(start_line=int(frame["line"]))
+            stacks = e["stack"] if isinstance(e["stack"], list) else [e["stack"]]
+            for stack in stacks:
+                frames = stack["frame"] if isinstance(stack["frame"], list) else [stack["frame"]]
+                for frame in frames:
+                    al = ArtifactLocation(uri=f'file://{frame["dir"]}/{frame["file"]}') if "dir" in frame.keys() else ArtifactLocation(uri=f'file://{frame["obj"]}', description=Message(text="No debug symbols found"))
+                    r = Region(start_line=int(frame["line"])) if "line" in frame.keys() else None
                     pl = PhysicalLocation(artifact_location=al, region=r)
-                ll = LogicalLocation(fully_qualified_name=frame["fn"])
-                l = Location(physical_location=pl, logical_locations=[ll])
-                result.locations.append(l)
+                    ll = LogicalLocation(fully_qualified_name=frame["fn"])
+                    l = Location(physical_location=pl, logical_locations=[ll])
+                    result.locations.append(l)
             results.append(result)
     else:
         print("No errors in XML input")
-
     
     run = Run(tool=tool, results=results)
     sarif.runs.append(run)
